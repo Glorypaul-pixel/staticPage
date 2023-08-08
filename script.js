0b46bf83ac59ab53ad481e20b677f473230ba102
Glorypaul-pixel <ifeomaglory1998@gmail.com>
2023-08-08 14:15:30 +0100

    i love react

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ const website = (
 );
 
 ReactDOM.render(website, document.getElementById("root"));
+
+function PageComponent() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <img src="./img/R.png" width="10%" />
+        </nav>
+      </header>
+      <h1>Reasons i am learning React</h1>
+      <ol>
+        <li>I love REACT</li>
+        <li>I love REACT</li>
+        <li>I love REACT</li>
+        <li>I love REACT</li>
+      </ol>
+      <footer>@20 Madubueze development. All right reserved.</footer>
+    </div>
+  );
+}
+ReactDOM.render(<PageComponent />, document.getElementById("header"));
