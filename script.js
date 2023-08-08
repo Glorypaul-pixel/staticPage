e9debcdfd1ec6b0f7d394d008a859a199295939f
Glorypaul-pixel <ifeomaglory1998@gmail.com>
2023-08-08 13:34:40 +0100

    static react app

---
+++ script.js
@@ -0,0 +1,15 @@
+const website = (
+  <div>
+    <h1>React app</h1>
+    <img src="./img/R.png" alt="react-logo" width="10%" />
+    <h2>React Learning Website</h2>
+    <ul>
+      <li> Was first released in 2013</li>
+      <li> Was originally created by Jordan Walke</li>
+      <li> Has well over 100k stars on Github</li>
+      <li>Is maintained by Facebook</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(website, document.getElementById("root"));
