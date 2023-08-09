b2b354c0f6958c3d803d2d283ba139e2f1b38952
Glorypaul-pixel <ifeomaglory1998@gmail.com>
2023-08-09 23:52:44 +0100

    did some changes

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-const website = (
-  <div>
-    <h1>React app</h1>
-    <img src="./img/R.png" alt="react-logo" width="10%" />
-    <h2>React Learning Website</h2>
-    <ul>
-      <li> Was first released in 2013</li>
-      <li> Was originally created by Jordan Walke</li> cf
-      <li> Has well over 100k stars on Github</li>
-      <li>Is maintained by Facebook</li>
-    </ul>
-  </div>
-);
-
-ReactDOM.render(website, document.getElementById("root"));
-
-ReactDOM.render(website, document.getElementById("root"));
 function Header() {
   return (
     <header>
@@ -28,7 +11,7 @@ function Details() {
   return (
     <div>
       <h1>Reasons i am learning React</h1>
-      <ol>
+      <ol className="nav-item">
         <li>I love REACT</li>
         <li>I love REACT</li>
         <li>I love REACT</li>
