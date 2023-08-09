e24bf40b263a3117b68ed884d970b42c91b2f653
Glorypaul-pixel <ifeomaglory1998@gmail.com>
2023-08-09 23:48:42 +0100

    iddann

--- script.js
+++ script.js
@@ -14,20 +14,6 @@ const website = (
 
 ReactDOM.render(website, document.getElementById("root"));
 
-const web = (
-  <div>
-    <h1>React app</h1>
-    <img src="./img/R.png" alt="react-logo" width="10%" />
-    <h2>React Learning Website</h2>
-    <ul>
-      <li> Was first released in 2013</li>
-      <li> Was originally created by Jordan Walke</li> cf
-      <li> Has well over 100k stars on Github</li>
-      <li>Is maintained by Facebook</li>
-    </ul>
-  </div>
-);
-
 ReactDOM.render(website, document.getElementById("root"));
 function Header() {
   return (
