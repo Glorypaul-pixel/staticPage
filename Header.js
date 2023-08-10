053765339c3a0337bad2241dbd3cd32fe8e0f06f
Glorypaul-pixel <ifeomaglory1998@gmail.com>
2023-08-10 23:36:34 +0100

    create a header in React

---
+++ Header.js
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <img src="./img/R.png" width="10%" />
+        <ul className="nav-item">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-function Header() {
-  return (
-    <header>
-      <nav className="nav">
-        <img src="./img/R.png" width="10%" />
-        <ul className="nav-item">
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
 function Details() {
   return (
     <div>
