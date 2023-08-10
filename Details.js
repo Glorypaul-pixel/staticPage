33f31a3f54c761aa21c9f1e16f45396c53b881b6
Glorypaul-pixel <ifeomaglory1998@gmail.com>
2023-08-10 23:40:50 +0100

    details

---
+++ Details.js
@@ -0,0 +1,13 @@
+export default function Details() {
+  return (
+    <div>
+      <h1>Reasons i am learning React</h1>
+      <ol>
+        <li>I love REACT</li>
+        <li>I love REACT</li>
+        <li>I love REACT</li>
+        <li>I love REACT</li>
+      </ol>
+    </div>
+  );
+}
--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-function Details() {
-  return (
-    <div>
-      <h1>Reasons i am learning React</h1>
-      <ol>
-        <li>I love REACT</li>
-        <li>I love REACT</li>
-        <li>I love REACT</li>
-        <li>I love REACT</li>
-      </ol>
-    </div>
-  );
-}
+import Header from "./Header";
+
 function Footer() {
   return (
     <div>
